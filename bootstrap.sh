d3958486bce4a1093462df0025fafb6c8b5c5ea5
Jon <gravityrideseverything1@gmail.com>
2023-03-05 18:40:45 -0500

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,13 +14,15 @@ install_k8s() {
 install_zsh() {
   apt install zsh -y
   chsh -s /usr/bin/zsh ubuntu
-  touch /home/ubuntu/.zshrc
   su ubuntu -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
 }
 
 install_postgres() {
-  apt install postgresql postgresql-contrib -y && systemctl start postgresql.service
+  apt install postgresql postgresql-contrib -y
+  systemctl enable postgresql.service --now
+  sed -i '/^local\s\+all\s\+postgres/s/peer/trust/' /etc/postgresql/14/main/pg_hba.conf
   echo "host    all             all             0.0.0.0/0               md5" | tee -a /etc/postgresql/14/main/pg_hba.conf
+  systemctl restart postgresql
 }
 
 install_deps
